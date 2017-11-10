d81491609a48b4fb5258bcbfed210742028e7279
KevinSites <KevinSites@KEVINSITES6290>
2017-11-10 15:59:19 -0700

    Updated freelance controller, bio page, and freelance index page.

--- Project1/Project1/Controllers/FreeLanceController.cs
+++ Project1/Project1/Controllers/FreeLanceController.cs
@@ -17,7 +17,36 @@ namespace Project1.Controllers
         // GET: FreelancerBio
         public ActionResult Bio()
         {
+            ViewBag.name = TempData["name"];
+            ViewBag.picture = TempData["picture"];
+            ViewBag.bio = TempData["bio"];
+            ViewBag.portfolio = TempData["portfolio"];
+            ViewBag.faqs = TempData["faqs"];
+
             return View();
+        }
+
+        public ActionResult Elevated()
+        {
+            TempData["name"] = "Elevated-Films";
+            TempData["picture"] = "Elivated Films full white.jpg";
+            TempData["bio"] = "A simple, professional, and affordable videography company for all of your events. " +
+                        "We offer a comprehensive experience to capture your moments from every angle. Reaching " +
+                        "new heights of event filmography, the sky truly is the limit.";
+            TempData["portfolio"] = "http://elevated-films.com";
+            TempData["faqs"] = "Elevated-Films";
+
+            return RedirectToAction("Bio");
+        }
+
+        public ActionResult Photographer()
+        {
+            return RedirectToAction("Bio");
+        }
+
+        public ActionResult Molly()
+        {
+            return RedirectToAction("Bio");
         }
     }
 }
