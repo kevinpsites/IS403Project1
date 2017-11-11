d70e949f1e6cdc4f6c8870f06f1e0c2edf715544
Joshua-Haines <josh.smc@gmail.com>
2017-11-10 20:12:58 -0700

    Josh's branch commit

--- Project1/Project1/Controllers/FreeLanceController.cs
+++ Project1/Project1/Controllers/FreeLanceController.cs
@@ -41,11 +41,34 @@ namespace Project1.Controllers
 
         public ActionResult Photographer()
         {
+            TempData["name"] = "Look It Photography";
+            TempData["picture"] = "profile.jpg";
+            TempData["bio"] = "I'm a Salt Lake County, Utah-based photographer who loves capturing the small and simple beauties " +
+                "of life. I believe that photography has emotion, and a power to capture the goodness of life. " +
+                "Photos allow people to experience and reflect on life in ways often overlooked in our rushed world. " +
+                "My passion is to capture details and stories through unique and compelling images. " +
+                "The world is an amazing place, and my hope is to uplift and enrich the lives of those around me.";
+            TempData["portfolio"] = "https://www.lookitphotography.com/";
+            TempData["faqs"] = "Look It Photography";
+
             return RedirectToAction("Bio");
         }
 
         public ActionResult Molly()
         {
+            TempData["name"] = "Molly in the Mineshaft";
+            TempData["picture"] = "Molly1.jpg";
+            TempData["bio"] = "Molly in the Mineshaft was founded in 2014 in Provo, Utah while its members " +
+                        "were attending college and performing with other bands. Their purpose is to " +
+                        "uplift, entertain, and inspire people of all ages through high-energy, diverse music. " +
+                        "The band’s music reveals their wide range of musical influences and features flavors " +
+                        "of contemporary folk, bluegrass, blues, jazz, Celtic, and rock. " +
+                        "Members of the band have performed throughout North America and Europe, " +
+                        "including the United States, Mexico, Croatia, France, Switzerland, and Spain. " +
+                        "Their audiences range from Elementary school children to international dignitaries.";
+            TempData["portfolio"] = "https://mollyinthemineshaft.com/";
+            TempData["faqs"] = "Molly in the Mineshaft";
+
             return RedirectToAction("Bio");
         }
     }
