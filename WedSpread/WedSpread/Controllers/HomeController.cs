4ad8c158635999db1ec414dd349676346e2caebd
Rachel Enniss <enniss.rachel@gmail.com>
2017-12-12 19:02:48 -0700

    change

--- WedSpread/WedSpread/Controllers/HomeController.cs
+++ WedSpread/WedSpread/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WedSpread.DAL;
+
 using WedSpread.Models;
 
 namespace Project1.Controllers
--- WedSpread/WedSpread/Global.asax.cs
+++ WedSpread/WedSpread/Global.asax.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using WedSpread.Models;
-using WedSpread.DAL;
+
 using System.Data.Entity;
 
 namespace WedSpread
