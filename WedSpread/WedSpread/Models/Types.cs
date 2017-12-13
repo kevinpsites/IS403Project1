18fec4d841184bc0dfc082a8bcfc7e79733561f0
KevinSites <KevinSites@KEVINSITES6290>
2017-12-12 17:51:39 -0700

    Add all models and connection to database

---
+++ WedSpread/WedSpread/Models/Types.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WedSpread.Models
+{
+    public class Types
+    {
+        public int TypeID { get; set; }
+        public string TypeName { get; set; }
+    }
+}
